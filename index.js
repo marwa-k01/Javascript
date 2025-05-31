1bffd77ac568965bcb5c861e6052c7cdaa7fb036
Marwa Kashify <marwakashify37@gmail.com>
2025-05-31 11:05:57 -0700

    Day 1
    Data types and comments completed

---
+++ index.js
@@ -0,0 +1,19 @@
+let firstName;
+firstName = "Marwa";
+console.log(firstName);
+
+let lastName = "Kashify";
+lastName = "Kashify";
+console.log(lastName);
+
+let postion = "Web Designer"
+console.log(postion);
+
+const age = 21;
+console.log(age);
+
+
+alert("Hello!");
+
+// alert("I am Marwa Kashify");
+// alert("This my task");
